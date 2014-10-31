cb1bfbbc5cc788af3e673473599510c011e0b617
JP Hastings-Spital <jphastings@blinkbox.com>
2014-10-31 10:46:29 +0000

    A little more documentation

--- lib/genny/uri.rb
+++ lib/genny/uri.rb
@@ -3,6 +3,8 @@ require "uri"
 
 module Genny
   module URI
+    # @param [Hash] opts Options for the generator
+    # @option opts [String] :host ('example.com') The host of the generated URI
     def self.genny(opts = {})
       opts = Genny.symbolize(opts)
       ::URI::HTTP.build(host: opts[:host] || 'example.com', path: "/#{Genny::String.genny}")
--- lib/genny/version.rb
+++ lib/genny/version.rb
@@ -1,4 +1,5 @@
 module Genny
+  # Grabbed from the VERSION file in the root of the gem
   VERSION = begin
     File.read(File.join(__dir__, "../../../VERSION")).strip
   rescue Errno::ENOENT
